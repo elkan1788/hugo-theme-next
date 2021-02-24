bd1fbfc19a8690f2de9f497e288f45cb0180d431
凡梦星尘 <elkan1788@gmail.com>
2021-02-24 16:49:49 +0800

    Use travis encrypt function.

--- .travis/replace_params_value.sh
+++ .travis/replace_params_value.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-pwd
+echo "Start replace the plugin's paramter"
 
 ## replace the paramter's values
-echo $LiveReId
+sed -i 's/Your LiveReId/$LiveReId/g' config/_default/params.toml
+sed -i 's/Your BShareId/$BShareId/g' config/_default/params.toml
+sed -i 's/Your RevolverMapId/$RevolverMapId/g' config/_default/params.toml
+sed -i 's/Your LCAppId/$LCAppId/g' config/_default/params.toml
+sed -i 's/Your LCAppKey/$LCAppKey/g' config/_default/params.toml
+sed -i 's/Your LCServer/$LCServer/g' config/_default/params.toml
+sed -i 's/Your CNNZSiteId/$CNNZSiteId/g' config/_default/params.toml
+sed -i 's/Your BaiduSiteId/$BaiduSiteId/g' config/_default/params.toml
+sed -i 's/Your GoogleSiteId/$GoogleSiteId/g' config/_default/params.toml
+sed -i 's/Your DaoVoiceId/$DaoVoiceId/g' config/_default/params.toml
+
+echo "Replace the plugin's paramter is success."
