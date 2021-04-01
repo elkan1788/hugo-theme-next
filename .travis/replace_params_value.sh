938249a74e4672ce5ba54a27fe8f252ba291c57a
凡梦星尘 <elkan1788@gmail.com>
2021-04-01 19:02:06 +0800

    fixed the replace params path

--- .travis/replace_params_value.sh
+++ .travis/replace_params_value.sh
@@ -3,6 +3,6 @@
 echo "Start replace the plugin's paramter"
 
 ## replace the paramter's values
-sed -i 's/Your AddthisId/'$AddthisId'/g;s/Your BaiduSiteId/'$BaiduSiteId'/g;s/Your CNNZSiteId/'$CNNZSiteId'/g;s/Your DaoVoiceId/'$DaoVoiceId'/g;s/Your GoogleSiteId/'$GoogleSiteId'/g;s/Your LCAppId/'$LCAppId'/g;s/Your LCAppKey/'$LCAppKey'/g;s|Your LCServer|'$LCServer'|g;s/Your LiveReId/'$LiveReId'/g;s/Your RevolverMapId/'$RevolverMapId'/g;s|Your WalineSerURL|'$WalineSerURL'|g' exampleSite/config/_default/params.toml
+sed -i 's/Your AddthisId/'$AddthisId'/g;s/Your BaiduSiteId/'$BaiduSiteId'/g;s/Your CNNZSiteId/'$CNNZSiteId'/g;s/Your DaoVoiceId/'$DaoVoiceId'/g;s/Your GoogleSiteId/'$GoogleSiteId'/g;s/Your LCAppId/'$LCAppId'/g;s/Your LCAppKey/'$LCAppKey'/g;s|Your LCServer|'$LCServer'|g;s/Your LiveReId/'$LiveReId'/g;s/Your RevolverMapId/'$RevolverMapId'/g;s|Your WalineSerURL|'$WalineSerURL'|g' config/_default/params.toml
 
 echo "Replace the plugin's paramter is success."
