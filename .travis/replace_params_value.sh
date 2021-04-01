60f6e55e8abcd21e9a33c638cbcb24651bd88811
凡梦星尘 <elkan1788@gmail.com>
2021-04-01 18:50:12 +0800

    fixed the replace parameters scripts

--- .travis/replace_params_value.sh
+++ .travis/replace_params_value.sh
@@ -3,15 +3,6 @@
 echo "Start replace the plugin's paramter"
 
 ## replace the paramter's values
-sed -i 's/Your LiveReId/'$LiveReId'/g' config/_default/params.toml
-sed -i 's/Your BShareId/'$BShareId'/g' config/_default/params.toml
-sed -i 's/Your RevolverMapId/'$RevolverMapId'/g' config/_default/params.toml
-sed -i 's/Your LCAppId/'$LCAppId'/g' config/_default/params.toml
-sed -i 's/Your LCAppKey/'$LCAppKey'/g' config/_default/params.toml
-sed -i 's/Your LCServer/'$LCServer'/g' config/_default/params.toml
-sed -i 's/Your CNNZSiteId/'$CNNZSiteId'/g' config/_default/params.toml
-sed -i 's/Your BaiduSiteId/'$BaiduSiteId'/g' config/_default/params.toml
-sed -i 's/Your GoogleSiteId/'$GoogleSiteId'/g' config/_default/params.toml
-sed -i 's/Your DaoVoiceId/'$DaoVoiceId'/g' config/_default/params.toml
+sed -i 's/Your AddthisId/'$AddthisId'/g;s/Your BaiduSiteId/'$BaiduSiteId'/g;s/Your CNNZSiteId/'$CNNZSiteId'/g;s/Your DaoVoiceId/'$DaoVoiceId'/g;s/Your GoogleSiteId/'$GoogleSiteId'/g;s/Your LCAppId/'$LCAppId'/g;s/Your LCAppKey/'$LCAppKey'/g;s|Your LCServer|'$LCServer'|g;s/Your LiveReId/'$LiveReId'/g;s/Your RevolverMapId/'$RevolverMapId'/g;s|Your WalineSerURL|'$WalineSerURL'|g' exampleSite/config/_default/params.toml
 
 echo "Replace the plugin's paramter is success."
