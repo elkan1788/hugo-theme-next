586f8f597e170a2ab8eccaa59aa7ef3fdeda0638
凡梦星尘 <elkan1788@139.com>
2022-04-12 18:36:23 +0800

    update the travis replace script.

--- .travis/replace_params_value.sh
+++ .travis/replace_params_value.sh
@@ -3,6 +3,18 @@
 echo "Start replace the plugin's paramter"
 
 ## replace the paramter's values
-sed -i 's/Your AddthisId/'$AddthisId'/g;s/Your BaiduSiteId/'$BaiduSiteId'/g;s/Your CNNZSiteId/'$CNNZSiteId'/g;s/Your DaoVoiceId/'$DaoVoiceId'/g;s/Your GoogleSiteId/'$GoogleSiteId'/g;s/Your LCAppId/'$LCAppId'/g;s/Your LCAppKey/'$LCAppKey'/g;s|Your LCServer|'$LCServer'|g;s/Your LiveReId/'$LiveReId'/g;s/Your RevolverMapId/'$RevolverMapId'/g;s|Your WalineSerURL|'$WalineSerURL'|g' config/_default/params.toml
+sed -i '
+s/Your AddthisPubid/'$AddthisPubid'/g
+s/Your BaiduSiteId/'$BaiduSiteId'/g
+s/Your CNNZSiteId/'$CNNZSiteId'/g
+s/Your DaoVoiceId/'$DaoVoiceId'/g
+s/Your GoogleSiteId/'$GoogleSiteId'/g
+s/Your LCAppId/'$LCAppId'/g
+s/Your LCAppKey/'$LCAppKey'/g
+s|Your LCServer|'$LCServer'|g
+s/Your LiveReId/'$LiveReId'/g
+s/Your RevolverMapId/'$RevolverMapId'/g
+s|Your WalineSerURL|'$WalineSerURL'|g
+' config/_default/params.toml 
 
 echo "Replace the plugin's paramter is success."
